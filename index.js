7ce289012aefc8d05c4f31e4e0c71ffd75a49c12
oluwamuibiclear <igbokweudoka@gmail.com>
2024-07-25 17:18:31 -0500

    project email js for form implementation

--- index.js
+++ index.js
@@ -2,3 +2,32 @@
 // service_75hbdx7
 // abn_Yb9GTeK-lhioj
 
+function contact(event) {
+    event.preventDefault();
+
+    const loading = document.querySelector('.modal__overlay--loading')
+    const succces = document.querySelector('.modal__overlay--succces')
+    loading.classList += ' modal__overlay--visible';
+    emailjs.sendForm(
+        'service_75hbdx7',
+        'template_hb9vdtf',
+        event.target,
+        'abn_Yb9GTeK-lhioj'
+    ).then(() => {
+        // console.log("it worked")
+        // throw new Error('error')
+        loading.classList.remove('modal__overlay--visible')
+        succces.classList += ' modal__overlay--visible'
+    }).catch(() => {
+        loading.classList.remove('modal__overlay--visible')
+        
+        succces.innerHTML = "The Email Service is temporarily unavailable. Please contact me directly on oluwamuibiigbokwe@gmail.com"
+        succces.style.fontSize = '30px';
+        succces.classList += ' modal__overlay--visible'
+        // console.log('it does not work')
+    })
+}
+
+function toggleModule(){
+    // toggle module
+}
